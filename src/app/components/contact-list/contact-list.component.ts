704e856e23c74805df2544c158f71c237a36287b
sam83045 <shoaib786k@gmail.com>
2018-11-25 10:47:57 +0530

    Contact List populated

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from '../../model/contact';
+import { ContactManagerService } from '../../services/contact-manager.service';
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
+  providers:[ContactManagerService]
 })
 export class ContactListComponent implements OnInit {
-
-  constructor() { }
+  public contacts: Contact[];
+  constructor(
+    private contactManagerService: ContactManagerService
+  ) { }
 
   ngOnInit() {
+    this.contactManagerService.getAllContacts().subscribe((contacts: Contact[]) => {
+      this.contacts = contacts;
+    })
   }
 
 }
--- src/app/components/contact-list/contact-list.module.ts
+++ src/app/components/contact-list/contact-list.module.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ContactListComponent } from "./contact-list.component";
 
 @NgModule({
-    declarations: [ContactListComponent]
+    declarations: [ContactListComponent],
+    imports: [CommonModule]
 })
 
 export class ContactListModule {
