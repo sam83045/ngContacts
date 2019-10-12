afa177750f835e53cd302849c510fdba8af69f84
sam83045 <shoaib786k@gmail.com>
2019-10-12 22:06:40 +0530

    API calls added to server.js

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const fs = require('file-system');
 
 const app = express();
+
+//Retrive Data from DB
+function getDBData() {
+    const dt = JSON.parse(fs.readFileSync('./db.json'));
+    console.log(dt);
+    return dt;
+}
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('/api/contacts', (req, res) => {
+    const allContacts = getDBData();
+    return res.status(200).json(allContacts.contacts);
+});
+
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    process.stdout.write(`The server is available on ${__dirname}:${PORT}`);
+});
